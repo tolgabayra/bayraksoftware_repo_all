4522511d2b0e7c3c908b12b40d1ebe699951f70d
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-01-28 12:22:19 +0300

    Mantine table added

---
+++ client/src/pages/dashboard/Products.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState } from 'react'
+import {
+    createStyles,
+    Table,
+    ScrollArea,
+    UnstyledButton,
+    Group,
+    Text,
+    Center,
+    TextInput,
+} from '@mantine/core';
+
+
+export default function Products() {
+    return (
+        <div>
+            <h4 className=' text-center text-xl mt-2'>Products</h4>
+            
+        </div>
+    )
+}
